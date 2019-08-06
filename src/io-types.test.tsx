e2d9b4f55a644ba2160f1dfa1521697d7c78ba96
Angelo Capone <angelo.capone@eng.it>
2019-08-06 11:18:03 +0200

    initial commit

---
+++ src/io-types.test.tsx
@@ -0,0 +1,81 @@
+import { MovieV, MovieDto, decodeToPromise } from "./io-types";
+import { PathReporter } from "io-ts/lib/PathReporter";
+import { none, some, Option, toUndefined, fold } from "fp-ts/lib/Option";
+import { isLeft } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
+
+/**
+ * [return on object literal (the outuput of a http://www.omdbapi.com/?apikey=[yourkey]&t=Chuck)]
+ * @return {[object]} [ return movie object literal]
+ */
+const getMovieLiteral = () => {
+  return { "Title": "Chuck", "Year": "2007–2012", "Rated": "TV-14", "Released": "24 Sep 2007", "Runtime": "43 min", "Genre": "Action, Comedy, Drama", "Director": "N/A", "Writer": "Chris Fedak, Josh Schwartz", "Actors": "Zachary Levi, Yvonne Strahovski, Joshua Gomez, Vik Sahay", "Plot": "When a twenty-something computer geek inadvertently downloads critical government secrets into his brain, the C.I.A. and the N.S.A. assign two agents to protect him and exploit such knowledge, turning his life upside down.", "Language": "English", "Country": "USA", "Awards": "Won 2 Primetime Emmys. Another 7 wins & 30 nominations.", "Poster": "https://m.media-amazon.com/images/M/MV5BMTM0ODIwNjI3NF5BMl5BanBnXkFtZTcwODU2ODUzMw@@._V1_SX300.jpg", "Ratings": [{ "Source": "Internet Movie Database", "Value": "8.2/10" }], "Metascore": "N/A", "imdbRating": "8.2", "imdbVotes": "120,989", "imdbID": "tt0934814", "Type": "series", "totalSeasons": "5", "Response": "True" };
+};
+
+/**
+ * [decode object literal decoding function]
+ * @type {Function}
+ */
+const decode = (movie: any): Option<MovieDto> => {
+  console.log("try to decode a movie ", movie);
+  const result = MovieV.decode(movie);
+  if (isLeft(result)) {
+    console.log(PathReporter.report(result));
+  }
+  return isLeft(result) ? none : some(result.right);
+};
+
+
+const printMessage = (movie: MovieDto) => {
+  console.log(`Successfully decoded movie with title ${movie.Title} and genres: ${movie.Genre}`);
+};
+
+describe("types checking step by step", () => {
+  it("try to decode a movie and call a typed function that handle a possible undefined parameter", () => {
+
+    const logMovie = jest.fn((movie: MovieDto | undefined) => {
+      if (!movie) {
+        return;
+      }
+      printMessage(movie);
+    });
+
+    const movie: MovieDto | undefined = toUndefined(decode(getMovieLiteral()));
+
+    expect(movie).not.toBe(undefined);
+
+    logMovie(movie);
+
+    expect(logMovie).toHaveReturned();
+
+  });
+
+  it("try to decode a movie and call a typed function. No undefined any more!", () => {
+
+    const _logMovie = jest.fn((_movie: MovieDto) => {
+      printMessage(_movie);
+    });
+
+    pipe(
+      decode(getMovieLiteral()),
+      fold(
+        () => {
+          expect(undefined).not.toBe(undefined);
+        },
+        (m: MovieDto) => {
+          _logMovie(m);
+
+          expect(_logMovie).toHaveReturned();
+          expect(m.Genre.length > 0).toBeTruthy();
+        },
+      ),
+    );
+
+  });
+
+  it("try to decode a movie getting a Promise. Wow!", () => {
+    const p: Promise<MovieDto> = decodeToPromise(MovieV, getMovieLiteral());
+    return expect(p).resolves.toMatchObject(expect.anything());
+  });
+
+});
---
+++ src/io-types.ts
@@ -0,0 +1,69 @@
+import * as t from "io-ts";
+
+import { either, fold } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
+import { reporter } from "io-ts-reporters";
+
+// original idea from https://github.com/hallettj/hacker-news-ts/blob/master/src/index.ts
+// Apply a validator and get the result in a `Promise`
+export function decodeToPromise<T, O, I>(
+	validator: t.Type<T, O, I>,
+	input: I,
+): Promise<T> {
+	const result = validator.decode(input);
+
+	return pipe(
+		validator.decode(input),
+		fold(
+			() => {
+				const messages = reporter(result);
+				return Promise.reject(new Error(messages.join("\n")));
+			},
+			(value: T) => Promise.resolve(value),
+		),
+	);
+}
+
+/**
+ * Custom type
+ *
+ * A value of type Type<A, O, I> (called "codec") is the runtime representation of the static type A.
+ * A codec can:
+ *  decode inputs of type I (through decode)
+ *  encode outputs of type O (through encode)
+ *  be used as a custom type guard (through is)
+ *
+ * @see https://github.com/gcanti/io-ts#custom-types
+ */
+const ArrayFromCommaSeparatedStrings = new t.Type<string[], string, unknown>(
+	"ArrayFromCommaSeparatedStrings",
+	(u): u is string[] => u instanceof Array,
+	(u, c) =>
+		either.chain(
+			t.string.validate(u, c),
+			(s: string) => {
+				const n = s.indexOf(",");
+				return n === -1 ? t.failure(u, c, "cannot parse to an array") : t.success(s.split(","));
+			}),
+	(a) => a.toString(),
+);
+
+/**
+ * [Movie Validator and Deserializer]
+ */
+export const MovieV = t.type({
+	Genre: ArrayFromCommaSeparatedStrings,
+	Rated: t.string,
+	Released: t.string,
+	Runtime: t.string,
+	Title: t.string,
+	Type: t.string,
+	Year: t.string,
+});
+
+/**
+ * [MovieDto data transfer object]
+ * @type {[type]}
+ */
+export type MovieDto = t.TypeOf<typeof MovieV>;
+
